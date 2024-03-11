db3d9a48ef16d3324a59c84ab33cc6f97bf632c3
Vincent10z <vincentsmithray@gmail.com>
2024-03-11 11:54:17 +0200

    Add CRUD command changeTodoItem

--- main.go
+++ main.go
@@ -67,9 +67,34 @@ func toggleTodoStatus(context *gin.Context) {
 	todo.Completed = !todo.Completed
 
 	context.IndentedJSON(http.StatusOK, todo)
-
-
 }
+
+func changeTodoItem(context *gin.Context) {
+	id := context.Param("id")
+	todo, err := getTodoByID(id)
+
+	if err != nil {
+		context.JSON(http.StatusNotFound, gin.H{"message": "Todo not found"})
+		return
+	}
+
+	var update struct {
+		Item string `json:"item"`
+	}
+	if err := context.BindJSON(&update); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid request"})
+		return
+	}
+
+	todo.Item = update.Item
+
+	context.IndentedJSON(http.StatusOK, todo)
+}
+
+
+
+
+
 func deleteTodoByID(context *gin.Context) {
 	id := context.Param("id")
 	_, err := getTodoByID(id)
@@ -93,7 +118,8 @@ func main() {
 	router := gin.Default()
 	router.GET("/todos", getTodos)
 	router.GET("/todos/:id", getTodo)
-	router.PATCH("/todos/:id", toggleTodoStatus)
+	router.PATCH("/todos/:id/status", toggleTodoStatus)
+	router.PATCH("/todos/:id/item", changeTodoItem)	
 	router.POST("/todos", addTodo)
 	router.DELETE("/todos/:id", deleteTodoByID)
 	router.Run("localhost:9090")
